cd6eba1fcb7e8af4d09c8f34116a92bea2303bab
paulayo93 <ayopaulofficial@gmail.com>
2020-08-05 08:48:38 +0100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import {} from '@wizdm/elements'
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
